99ce6cac8eaceaa971571ead884686fbeace2ab8
Carolina Negrin <caro_negrin@hotmail.com>
2023-07-03 19:25:10 -0300

    creando control de usuarios

--- project/apps/usuario/views.py
+++ project/apps/usuario/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Usuario
+
+def home (request):
+    usuarios_registrados = Usuario.objects.all()
+    contexto = {"usuarios" : usuarios_registrados}
+    render(request, "usuario/index.html", contexto)
