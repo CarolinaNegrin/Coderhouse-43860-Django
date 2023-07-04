15d85d3a2a4aa3e1af717b4fcd467f03de95d9f0
Carolina Negrin <caro_negrin@hotmail.com>
2023-07-04 20:59:41 -0300

    final de clase

--- project/apps/cliente/urls.py
+++ project/apps/cliente/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import home, crear_clientes
+from .views import home, crear_clientes, crear_cliente, busqueda
 
 app_name = "cliente"       
 
 urlpatterns = [
     path("", home, name="home" ),
-    path("crear_clientes", crear_clientes, name="crear_clientes")
+    path("crear_clientes", crear_clientes, name="crear_clientes"),
+    path("crear_cliente", crear_cliente, name="crear_cliente"),
+    path("busqueda/", busqueda, name="busqueda"),   
 ]
 
--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Cliente, Pais
-
+from .forms import ClienteForm
+from datetime import date
 def home(request):
     clientes_registros = Cliente.objects.all()
     contexto =  {"clientes": clientes_registros}
@@ -10,7 +11,7 @@ def home(request):
     return render(request, "cliente/index.html", contexto)
 
 def crear_clientes(request):
-    from datetime import date
+    
     p1 = Pais(nombre="Perú")
     p2 = Pais(nombre="Argentina")
     p3 = Pais(nombre="Italia")
@@ -29,3 +30,26 @@ def crear_clientes(request):
     c4.save()
     
     return redirect("cliente:home")
+
+def crear_cliente(request) :
+    if request.method == "POST" :
+        form = ClienteForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("cliente:home")
+    else: #request.method == "GET"
+        form = ClienteForm()
+    return render(request, "cliente/crear.html",{'form':form} )
+    
+def busqueda(request):
+    #Busqueda por nombre que contenga "al"
+    cliente_nombre = Cliente.objects.filter(nombre__contains="al")
+    #Busqueda por año nacimiento mayor a 2000
+    cliente_nacimiento = Cliente.objects.filter(nacimiento__gt=date(2000,1,1))
+    #Pais de origen vacío
+    cliente_pais = Cliente.objects.filter(pais_origen_id=None)
+
+    contexto = {"cliente_nombre":cliente_nombre,
+                "cliente_nacimiento": cliente_nacimiento,
+                "cliente_pais":cliente_pais}
+    return render(request, "cliente/busqueda.html", contexto)
